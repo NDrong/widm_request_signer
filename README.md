2e2d724b0094de2f2a08913d2a2988e642061c9a
Noam Drong <noam@drong.net>
2021-01-02 01:23:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## 'Wie is de Mol?' API Request Signer
+This repository contains a simple Python 3 application that takes a raw HTTP request from `./signature`
+and signs it using the HMAC algorithm (SHA-256) used by the 'Wie is de Mol?' Android app and its API. See 
+[this article](https://n04m.nl) on my blog for more details on the background behind this utility.
+
+### Why?
+I was interested in reverse engineering the API behind the popular 'Wie is de Mol?' app. When I started reversing
+the Android app, I noticed that requests are signed, with the signatures being verified by the backend. In order to 
+execute arbitrary request against the API, I needed a tool that would sign requests using the app's hardcoded secret.
+
+### Where?
+You can find the API on `api.wieisdemol.nl`. So far, I've come across the following routes while using the Android 
+application:
+
+- `/config/`
+- `/profile/`
+- `/profile/forgot_password/`
+
+I'll add more details when I have the time later (as it's currently already past 1 AM...)
+
+### License
+See [`LICENSE`]() for more information.
---
+++ sign_request.py
@@ -0,0 +1,42 @@
+import hashlib
+import hmac
+import base64
+import email
+
+SECRET = bytes('█████', 'utf-8')
+
+
+def get_message_for_request(request: str) -> bytes:
+    # Parse the HTTP request.
+    headers, body = request.split('\n\n')
+
+    method, path, _ = headers.split(' ', 2)
+
+    # Drop the final newline from the body.
+    body = body[:-1]
+
+    headers = headers.split('\n')[1:]
+    header_lookup = dict()
+    for header in headers:
+        name, value = header.split(': ', 1)
+        header_lookup[name] = value
+
+    # Compose the message.
+    body_hashed = hashlib.sha256(body.encode('utf-8')).hexdigest()
+
+    message = f'{method}\n{body_hashed}\n{header_lookup["Content-Type"]}\n{header_lookup["Date"]}\n{path}'
+
+    return bytes(message, 'utf-8')
+
+
+def entry() -> None:
+    with open('request') as file:
+        message = get_message_for_request(file.read())
+
+    signature = base64.b64encode(hmac.new(SECRET, message, digestmod=hashlib.sha256).digest())
+
+    print(f'X-Signature: HMAC widm-api:{signature.decode()}')
+
+
+if __name__ == "__main__":
+    entry()
